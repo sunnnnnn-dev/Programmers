d0eab5d2d5ee33595c9866a0df37e74de18a2e0f
sunnnu <56190132+sunnnu@users.noreply.github.com>
2019-12-01 01:47:36 +0900

    Update 문자열내림차순으로배치하기.py

--- Programmers/LV_1/문자열내림차순으로배치하기.py
+++ Programmers/LV_1/문자열내림차순으로배치하기.py
@@ -1,9 +1,2 @@
 def solution(s):
-    res = [i for i in s.encode()]
-    res.sort(reverse=True)
-
-    res2 = ''
-    for i in res:
-        res2 += chr(i)
-
-    return res2
+    return ''.join(sorted(s, reverse=True))
