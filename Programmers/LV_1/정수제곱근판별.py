d83db6fde3e43f93d29c7a4137e42144c5e4ca79
sunnnu <56190132+sunnnu@users.noreply.github.com>
2019-12-06 17:48:07 +0900

    Update 정수제곱근판별.py

--- Programmers/LV_1/정수제곱근판별.py
+++ Programmers/LV_1/정수제곱근판별.py
@@ -1,8 +1,5 @@
 def solution(n):
-    for i in range(1,n//2+2):
-        if i**2 == n:
-            return (i+1)**2
-        elif n > i**2 and n < (i+1)**2:
-            return -1
-        else:
-            continue
+    sqrt = n**(1/2)
+    if sqrt % 1 == 0:
+        return (sqrt+1)**2
+    return -1
