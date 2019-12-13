f5a04b0ed00c6a90074150c2187d8394c071308e
sunnnu <56190132+sunnnu@users.noreply.github.com>
2019-12-13 17:22:19 +0900

    Create 예산.py

---
+++ Programmers/LV_1/예산.py
@@ -0,0 +1,9 @@
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    for i in d:
+        budget = budget-i
+        if budget<0:
+            break;    
+        answer += 1
+    return answer
