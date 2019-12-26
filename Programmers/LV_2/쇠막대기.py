4d0e02972b189394168ceda5aec3cbe948e1c919
sunnnu <56190132+sunnnu@users.noreply.github.com>
2019-12-26 15:48:42 +0900

    Update 쇠막대기.py
    시간초과 문제를 해결한 코드

--- Programmers/LV_2/쇠막대기.py
+++ Programmers/LV_2/쇠막대기.py
@@ -1,14 +1,13 @@
 def solution(arr):
     arr = arr.replace('()','8')
-    stick = []
     cnt = 0
+    bar = 0
     for i in arr:
         if i == '(':
-            stick.insert(0,i)
+            bar += 1
         elif i == '8':
-            stick.insert(0,i)
+            cnt += 1*bar
         else:
-            idx = stick.index('(')
-            cnt += stick[:idx].count('8')+1
-            del stick[idx]
+            bar -= 1
+            cnt += 1
     return cnt
